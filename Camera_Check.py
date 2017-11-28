c14a25a3a68f93ac02744610491373aa16c75fb3
Lars Nielsen <lars.nielsen@blusense-diagnostics.com>
2017-11-28 15:06:46 +0100

    Version 0.1

--- Camera_Check.py
+++ Camera_Check.py
@@ -2,6 +2,12 @@ import numpy as np
 import time
 import argparse
 import json
+from PIL import Image
+import cv2
+import os
+
+__version__ = 0.1
+
 
 """Get image paths from arguments"""
 
@@ -23,6 +29,13 @@ def create_return_dict():
     ret_dict['Error'] = ''
     ret_dict['nr_of_images'] = 0
     ret_dict['image_list'] = []
+    ret_dict['B_G_R_values'] = []
+    ret_dict['B_G_R_background_values'] = []
+    ret_dict['Image_size'] = []
+    ret_dict['Camera_QC_passed'] = False
+    ret_dict['Background_Image'] = ''
+    ret_dict['Version'] = __version__
+
     return ret_dict
 
 
@@ -31,7 +44,41 @@ def create_return_dict():
 
 def image_check(image_path_list, ret_dict):
     ret_dict['nr_of_images'] = len(image_path_list)
-    ret_dict['image_list'] = []  # image_path_list
+    ret_dict['image_list'] = image_path_list
+    if len(image_path_list) > 0:
+        background = image_path_list[0].replace('11.jpg', '10.jpg')
+        ret_dict['Background_Image'] = background
+        if os.path.isfile(background):
+            for item in image_path_list:
+                if os.path.isfile(item):
+                    im = cv2.imread(item)
+                    ret_dict['Image_size'].append([im.shape[0], im.shape[1]])
+                    B_G_R_values = [np.average(im[0:479, 170:300, 0]), np.average(im[0:479, 170:300, 1]), np.average(im[0:479, 170:300, 2])]
+                    ret_dict['B_G_R_values'].append(B_G_R_values)
+                else:
+                    ret_dict['Error'] = ret_dict['Error'] + item + ' does not exits\n'
+        else:
+            ret_dict['Error'] = ret_dict['Error'] + background + ' does not exits\n'
+    else:
+        ret_dict['Error'] = 'Not 3 Images supplied'
+    if ret_dict['Error'] == '':
+        QC_sum = 0
+        im_nr = 0
+        for item in ret_dict['B_G_R_values']:
+            im_background = cv2.imread(background)
+            ret_dict['B_G_R_background_values'] = [np.average(im_background[0:479, 170:300, 0]), np.average(
+                im_background[0:479, 170:300, 1]), np.average(im_background[0:479, 170:300, 2])]
+            if item[im_nr] > ret_dict['B_G_R_background_values'][im_nr] * 100:
+                QC_sum += 1
+                im_nr += 1
+        if QC_sum == len(ret_dict['B_G_R_values']):
+            ret_dict['Camera_QC_passed'] = True
+
+
+def delete_images(ret_dict):
+    os.remove(ret_dict['Background_Image'])
+    for item in ret_dict['image_list']:
+        os.remove(item)
 
 
 if __name__ == '__main__':
@@ -43,7 +90,6 @@ if __name__ == '__main__':
 
     image_check(image_list, return_dict)
 
-    #return_dict['image_list'] = image_list
-    #return_dict['nr_of_images'] = len(image_list)
-
     print json.dumps(return_dict)
+
+    delete_images(return_dict)
